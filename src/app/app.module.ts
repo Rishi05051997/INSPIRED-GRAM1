04879457186ae2103ca7f086510a5ff13f6ab675
vrushabhdhatrak <vrushabhdhatral10@gmail.com>
2021-02-20 08:25:16 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { UserComponent } from './components/user/user.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddpostComponent } from './pages/addpost/addpost.component';
+import { PageNotFOundComponent } from './pages/page-not-found/page-not-found.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    UserComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AddpostComponent,
+    PageNotFOundComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
